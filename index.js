7778567a0ce50728c1429217c8063c4977610ea1
integralz <luckystay12@gmail.com>
2021-05-21 03:04:00 +0900

    develop_branch

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser'); 
 const app = express();
 
-mongoose.set('useNewUrlParser', true);   
-mongoose.set('useFindAndModify', false); 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect('mongodb+srv://integralz:wnstjr12@cluster0.h9voc.mongodb.net/internet_board?retryWrites=true&w=majority');
-const db = mongoose.connection; 
+const db = mongoose.connection;
 
+app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
-db.once('open', function(){
-  console.log('DB connected');
+const contactschema = mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    email: { type: String },
+    phone: { type: String }
 });
 
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
+const contact = mongoose.model('contact', contactschema);
+
+db.once('open', function () {
+    console.log('DB connected');
+});
+
+db.on('error', function (err) {
+    console.log('DB ERROR : ', err);
+});
+
+app.get('/', function(req,res){
+    res.redirect('/ejs');
 });
 
 const port = 3000;
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('server on!' + port);
 });
